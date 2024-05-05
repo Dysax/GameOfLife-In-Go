64971b097397e66ec273cf02054c68efc2b9a193
Dysax <lukedysax@gmail.com>
2024-05-05 15:02:08 -0500

    render module added with RenderBoard and DrawLine functions

--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 
 	"github.com/Dysax/GameOfLife/pkg/board"
+	"github.com/Dysax/GameOfLife/pkg/render"
 )
 
 func main() {
-	w, h := 5, 5
+	w, h := 20, 10
 
 	boardState := board.RandomState(w, h)
 
 	for _, row := range boardState {
 		fmt.Println(row)
 	}
+	// fmt.Println(len(boardState[0]))
+	// fmt.Printf("board state array item %d\n", boardState[0][0])
+	render.RenderBoard(boardState)
 }
---
+++ pkg/render/render.go
@@ -0,0 +1,37 @@
+package render
+
+import (
+	"fmt"
+)
+
+// draws the entire board
+func RenderBoard(array [][]int) {
+	// top boarder
+	for k := 0; k < len(array[0])+2; k++ {
+		fmt.Printf("_")
+	}
+	fmt.Println()
+
+	for i := 0; i < len(array); i++ {
+		fmt.Printf("|")
+		DrawLine(array[i])
+		fmt.Printf("|\n")
+	}
+
+	for k := 0; k < len(array[0])+2; k++ {
+		fmt.Printf("-")
+	}
+	fmt.Println()
+}
+
+// draws an individual sub array
+func DrawLine(array []int) {
+
+	for i := 0; i < len(array); i++ {
+		if array[i] == 1 {
+			fmt.Printf("#")
+		} else {
+			fmt.Printf(" ")
+		}
+	}
+}
